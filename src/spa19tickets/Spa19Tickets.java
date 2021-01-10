2b4569d705af55088d192bf27f4546aa8e3a2b51
Daniel_Gómez_G <dgomez.informatica@gmail.com>
2021-01-10 16:55:59 -0300

    se pide los datos al usuario

--- src/spa19tickets/Spa19Tickets.java
+++ src/spa19tickets/Spa19Tickets.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import javax.swing.JOptionPane;
 import assets.ThemeWindow;
+import java.io.IOException;
+import resources.ConexionBD;
 
 /**
  
@@ -12,12 +14,20 @@ import assets.ThemeWindow;
  */
 public class Spa19Tickets {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ThemeWindow tm = new ThemeWindow();
         tm.WindowTheme();
+        ConexionBD x = new ConexionBD();
+        x.getConexion();
         
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-//        JOptionPane.showMessageDialog(null, "!!Hola Mundo", "Spa19Tickets", JOptionPane.INFORMATION_MESSAGE);
+        String rut, pass;
         
+        System.out.println("#### Authentificacion ####");
+        System.out.print("Usuario: ");
+        rut = bf.readLine();
+        System.out.print("Contraseña: ");
+        pass = bf.readLine();
+        JOptionPane.showMessageDialog(null, "Usuario: "+rut+"\nClave: "+pass, "Informacion Recibida - Spa19Tickets", JOptionPane.INFORMATION_MESSAGE);
     }  
 }
