d46605a7a034610cc63f682e4a65305b0aaf11fc
Daniel_Gómez_G <dgomez.informatica@gmail.com>
2021-01-10 13:54:09 -0300

    Cambio no aplicable al sistema [file main]

--- src/spa19tickets/Spa19Tickets.java
+++ src/spa19tickets/Spa19Tickets.java
@@ -1,14 +1,23 @@
 package spa19tickets;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import javax.swing.JOptionPane;
+import assets.ThemeWindow;
+
 /**
  
- * @author ites_
+ * @author DAGG
  * @version 1.0
  */
 public class Spa19Tickets {
 
     public static void main(String[] args) {
+        ThemeWindow tm = new ThemeWindow();
+        tm.WindowTheme();
         
-    }
-    
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//        JOptionPane.showMessageDialog(null, "!!Hola Mundo", "Spa19Tickets", JOptionPane.INFORMATION_MESSAGE);
+        
+    }  
 }
